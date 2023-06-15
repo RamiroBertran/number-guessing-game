7e64697b62a69d65f9d31aa29232a10400ff72cb
RamiroBertran <ramirobertra@gmail.com>
2023-06-14 21:08:24 -0300

    feat: add number_guess.sh script to folder

---
+++ number_guess.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Program that let user guess number between one and a thousand. Then the info games is stored in a database called number_guess 
+echo Enter your username:
+read USERNAME
+echo $USERNAME
